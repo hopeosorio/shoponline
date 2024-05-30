6ea3e6c7c6d96aab7d021c1d072286fc26a5f0a1
hopeosorio <ordonezosoriocesar@gmail.com>
2024-05-29 20:40:56 -0600

    Primer commit

---
+++ caoo-shop/src/app/primeng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const  modPrime: any = []
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    modPrime
+  ],
+  exports:[
+    modPrime
+  ]
+})
+export class PrimengModule { }
